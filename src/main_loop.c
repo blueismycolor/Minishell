bf19ef87f6ec38e95e53730cfa10271a4aa24681
Enzo Gatien <egatien@c1r7p11.42lehavre.fr>
2025-07-02 15:50:53 +0200

    remove

--- src/main_loop.c
+++ src/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: egatien <egatien@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:23:37 by tlair             #+#    #+#             */
-/*   Updated: 2025/07/02 15:43:42 by egatien          ###   ########.fr       */
+/*   Updated: 2025/07/02 15:50:36 by egatien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ char	*init_main_loop_iteration(t_data *data)
 		print_prompt_header();
 		input = readline("\001\033[1;92m\002minishell> \001\033[0m\002");
 	}
-	else if (g_signal == SIGINT)
-		data->return_value = 130;
 	data->saved_stdin = dup(STDIN_FILENO);
 	data->saved_stdout = dup(STDOUT_FILENO);
 	return (input);
